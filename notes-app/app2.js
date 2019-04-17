97bf932813fdabaef6cd3cfaab3afbac0cda1bc3
ht-lim <ht.lim@navercorp.com>
2019-04-17 22:58:41 +0900

    add chapter 3: node.js module system(notes app)

--- notes-app/app2.js
+++ notes-app/app2.js
@@ -12,3 +12,25 @@ console.log(sum)
 // 4. From app.js, load in adnd call the function priting message 
 const notes = require('./notes.js')
 console.log(notes())
+
+const validator = require('validator')
+
+console.log(validator.isEmail('asdf@email.com'))
+
+console.log(validator.isURL('https:/url.io'))
+
+
+// Challenge: Use the chalk library in your project
+//
+// 1. Install version 2.4.1 of chalk
+// 2. Load chalk into app2.js
+// 3. Use it to print the string "Success!" to the console in green
+// 4. Test your work
+// 
+
+const chalk = require('chalk')
+const log = console.log
+
+log(chalk.green('Success!'));
+log(chalk.red.underline.inverse('Underline success!'));
+log(chalk.blue.inverse('Failure!'));
